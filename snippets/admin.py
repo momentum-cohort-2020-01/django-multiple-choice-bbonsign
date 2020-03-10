683c68ff066d3230e41ff6317c0932cb409a09e2
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-09 23:00:28 -0400

    Model fixes

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Tag, Snippet
+from .models import Tag, Snippet, Language
 
 
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
+class LanguageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Snippet)
+admin.site.register(Language, LanguageAdmin)
