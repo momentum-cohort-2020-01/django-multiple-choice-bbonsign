f107060d7cd40e3dfac4808f86efbd9c6e7ac17c
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-12 00:27:57 -0400

    Adds description field to form

--- snippets/forms.py
+++ snippets/forms.py
@@ -7,4 +7,4 @@ class SnippetForm(forms.ModelForm):
 
     class Meta:
         model = Snippet
-        fields = ('title', 'language', 'code', 'tags', 'public',)
+        fields = ('title', 'language', 'code', 'description', 'tags', 'public',)
